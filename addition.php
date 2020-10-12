15097dbef80d1f30869c6cf7b46a135cb3e7b0cf
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-10-12 15:09:19 +0530

    Addition Of 2 Numbers

---
+++ addition.php
@@ -0,0 +1,15 @@
+<!--
+
+Function Name    :  Addition Of 2 Numbers
+Function Date    :  12 Oct 2020
+Function Author  :  Prasad Dangare
+Output           :  Sum : 45
+
+-->
+
+<?php $x = 15;
+
+$y = 30;
+$z = $x + $y;
+
+echo "Sum: " . $z; ?>
