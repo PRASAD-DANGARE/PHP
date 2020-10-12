10fe1dc5e596e9ee0638d62267292b9de46c852e
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-10-12 15:09:49 +0530

    Find Area Of Rectangle

---
+++ area.php
@@ -0,0 +1,14 @@
+<!--
+
+Function Name    :  Find Area Of Rectangle
+Function Date    :  12 Oct 2020
+Function Author  :  Prasad Dangare
+Output           :  Area Of Rectangle Is 14 * 12 = 168
+
+-->
+
+<?php $length = 14;
+
+$width = 12;
+
+echo "Area Of Rectangle Is $length * $width = " . ($length * $width) . "<br />"; ?>
