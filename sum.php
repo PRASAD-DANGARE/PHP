e7c3ea53ce4099b398458ac79e2bfdbdee0a44b1
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-10-12 15:13:09 +0530

    Find The Area With Base And Height

---
+++ sum.php
@@ -0,0 +1,14 @@
+<!--
+
+Function Name    :  Find The Area With Base And Height
+Function Date    :  12 Oct 2020
+Function Author  :  Prasad Dangare
+Output           :  Area With Base 10 And Height 15 = 75
+
+-->
+
+<?php $base = 10;
+
+$height = 15;
+
+echo "Area With Base $base And Height $height = " . ($base * $height) / 2;
