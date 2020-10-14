67b1c518d8c289795b9a169ffb0e62d86b90105c
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-10-14 22:26:16 +0530

    Reverse A Given String

---
+++ revstring.php
@@ -0,0 +1,14 @@
+<!--
+
+Function Name    :  Reverse A Given String
+Function Date    :  14 Oct 2020
+Function Author  :  Prasad Dangare
+Output           :  Reverse String Of PRASAD Is DASARP
+
+-->
+
+
+<?php
+
+$string = "PRASAD";
+echo "Reverse String Of $string Is " . strrev($string);
