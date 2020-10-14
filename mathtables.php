1299f08ed4bc59eebade70864b5306406554b85a
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-10-14 22:04:17 +0530

    PHP Program To Write Table Of 9

---
+++ mathtables.php
@@ -0,0 +1,16 @@
+<!--
+
+Function Name    :  PHP Program To Write Table Of 9
+Function Date    :  14 Oct 2020
+Function Author  :  Prasad Dangare
+Output           :  9 18 27 36 45 54 63 72 81 90
+
+-->
+
+<?php
+
+define('a', 9);
+for ($i = 1; $i <= 10; $i++) {
+    echo $i * a;
+    echo '<br>';
+}
