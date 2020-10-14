9e4cf6056bb446db46184c20c9a161fe2bdada13
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-10-14 22:38:55 +0530

    Subtraction Of Two Numbers

---
+++ subtraction.php
@@ -0,0 +1,15 @@
+<!--
+
+Function Name    :  Subtraction Of Two Numbers
+Function Date    :  14 Oct 2020
+Function Author  :  Prasad Dangare
+Output           :  Difference After Subtraction : 50
+
+-->
+
+<?php
+
+$x = 100;
+$y = 50;
+$z = $x - $y;
+echo "Difference After Subtraction : ", $z;
