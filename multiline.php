907988a7ebb382917b9d01f59ee7c29d55619050
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-10-22 16:42:20 +0530

    PHP Program To Understand Use Of Multiline Comment

---
+++ multiline.php
@@ -0,0 +1,18 @@
+<!--
+
+Function Name    :  PHP Program To Understand The Use Of Multiline Comments
+Function Date    :  22 Oct 2020
+Function Author  :  Prasad Dangare
+Output           :  Bill Gates
+
+-->
+
+<?php
+
+$author = "Bill Gates";
+
+$text = "Measuring Programming Progress By Lines Of Code Is Like Measuring Aircraft Building Progress By Weight.
+
+-$author.";
+
+echo $author;
