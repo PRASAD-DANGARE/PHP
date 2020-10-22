29b709374e617f00e5e70df58d54b79843d9d881
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-10-22 15:53:20 +0530

    PHP Program To Print Name

---
+++ prasad.php
@@ -0,0 +1,17 @@
+<!--
+
+Function Name    :  PHP Program To Print Name
+Function Date    :  22 Oct 2020
+Function Author  :  Prasad Dangare
+Output           :  Prasad Dangare
+                 :  Prasad Dangare
+
+-->
+
+<?php
+$username = "Prasad Dangare";
+echo $username;
+echo "<br>";
+
+$current_user = $username;
+echo $current_user;
