4597f504357edf2dbc1401a7ffa917a5ca9b20ab
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-10-22 17:55:57 +0530

    Automatic Conversion From Number To String

---
+++ conversion.php
@@ -0,0 +1,13 @@
+<!--
+
+Function Name    :  PHP Program To Automatic Conversion From Number To String
+Function Date    :  22 Oct 2020
+Function Author  :  Prasad Dangare
+Output           :  7
+-->
+
+<?php
+
+$number = 1234 * 6789; // 8377626
+
+echo substr($number, 3, 1); // 3 is count from 0 to 3 is 837, and 1 is the next number which is 7.
