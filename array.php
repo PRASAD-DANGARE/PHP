cb19c05f1ba7b8a99648c879f9fa499483110d76
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-10-22 16:17:36 +0530

    PHP Program To Display 2-D Array

---
+++ array.php
@@ -0,0 +1,25 @@
+<!--
+
+Function Name    :  PHP Program To Display 2-D Array
+Function Date    :  22 Oct 2020
+Function Author  :  Prasad Dangare
+Output           :  abcdefghi
+
+-->
+
+<?php
+$team = array(
+    array('a', 'b', 'c'),
+    array('d', 'e', 'f'),
+    array('g', 'h', 'i')
+);
+
+echo $team[0][0]; // a
+echo $team[0][1]; // b
+echo $team[0][2]; // c
+echo $team[1][0]; // d
+echo $team[1][1]; // e
+echo $team[1][2]; // f
+echo $team[2][0]; // g
+echo $team[2][1]; // h
+echo $team[2][2]; // i
