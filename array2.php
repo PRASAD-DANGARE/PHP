62de49e3968d5e0983a7f11caf05a66a5b20ca97
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-10-23 15:44:49 +0530

    Index Array

---
+++ array2.php
@@ -0,0 +1,22 @@
+<!--
+
+Function Name    :  Index Array
+Function Date    :  23 Oct 2020
+Function Author  :  Prasad Dangare
+Output           :  My
+                 :  Self
+                 :  Prasad Dangare
+
+-->
+
+
+<?php
+
+$name = array("My", "Self", "Prasad Dangare");
+echo $name[0];
+echo "<br>";
+
+echo $name[1];
+echo "<br>";
+
+echo $name[2];
