e3171cdf5797baab853ab484d2a5392af2fd5791
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-10-23 14:16:54 +0530

    PHP Program To Converting A String To A Numbers

---
+++ conversion2.php
@@ -0,0 +1,14 @@
+<!--
+
+Function Name    :  PHP Program To Converting A String To A Numbers
+Function Date    :  23 Oct 2020
+Function Author  :  Prasad Dangare
+Output           :  78.5398175
+-->
+
+<?php
+
+$pi = "3.1415927";
+
+$radius = 5;
+echo $pi * ($radius * $radius);
