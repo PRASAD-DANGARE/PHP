d035479cd1bb0118dbedb4dd7b97b8d67e3649dd
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-10-23 15:19:13 +0530

    PHP Program To Understand The PHP DataTypes

---
+++ datatype.php
@@ -0,0 +1,34 @@
+<!--
+
+Function Name    :  PHP Program To Understand The PHP DataTypes
+Function Date    :  23 Oct 2020
+Function Author  :  Prasad Dangare
+Output           :  11
+                 :  Hello
+                 :  Prasad Dangare
+                 :  3.14
+                 
+-->
+
+<?php
+
+$i = NULL; // Null No Memory
+
+$j = 11; // int
+echo $j;
+echo "<br>";
+
+$str = "Hello"; // String
+echo $str;
+echo "<br>";
+
+$name = 'Prasad Dangare'; // String
+echo $name;
+echo "<br>";
+
+$f = 3.14; // Float
+echo $f;
+echo "<br>";
+
+$b = true; // Boolean
+?>
