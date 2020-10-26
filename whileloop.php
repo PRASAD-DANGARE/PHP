ca99ca03fc6cbc2b2f6e8f8130bbbe469242101e
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-10-26 20:11:24 +0530

    while Loop In PHP

---
+++ whileloop.php
@@ -0,0 +1,19 @@
+<!--
+
+Function Name    :  while Loop In PHP 
+Function Date    :  26 Oct 2020
+Function Author  :  Prasad Dangare
+Output           :  $ $ $ $ $
+
+-->
+
+<?php
+
+echo ("Pattern Printing") . '<br /><br />';
+$i = 0;
+
+while ($i < 5) {
+
+    echo (" $ ");
+    $i++;
+}
