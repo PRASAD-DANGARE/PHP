337b2a2496e6a17993375b4d9abed79650479aee
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-10-26 19:23:29 +0530

    File Inclusion In PHP Using require

---
+++ inclusion2.php
@@ -0,0 +1,24 @@
+<!--
+
+Function Name    :  File Inclusion In PHP Using require
+Function Date    :  26 Oct 2020
+Function Author  :  Prasad Dangare
+Output           :  If We Use require Method And If The File Is Not Available 
+                 :  Then require Method Generates Error And Abort The Execution Of Our Program
+
+-->
+
+
+<html>
+
+<body>
+
+    <?php
+
+    require("menu2.php");
+
+    ?>
+
+</body>
+
+</html>
