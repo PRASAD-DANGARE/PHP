482e6caf83194a43dfe650e54495e3f17fe33467
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-10-26 19:16:35 +0530

    File Inclusion In PHP Using include

---
+++ inclusion.php
@@ -0,0 +1,22 @@
+<!--
+
+Function Name    :  File Inclusion In PHP Using include
+Function Date    :  26 Oct 2020
+Function Author  :  Prasad Dangare
+Output           :  This Method include The File, If The File Is Not Available
+                 :  Then It Will Display The Warning Only And The Execution Of Our Program Get Continue
+
+-->
+
+<html>
+
+<body>
+
+    <?php
+
+    include("menu.php");
+    ?>
+
+</body>
+
+</html>
