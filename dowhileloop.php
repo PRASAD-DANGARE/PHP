862b27d3246e1dacca9b211574e951c2e614dd42
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-10-26 20:16:28 +0530

    do-while Loop In PHP

---
+++ dowhileloop.php
@@ -0,0 +1,18 @@
+<!--
+
+Function Name    :  do-while Loop In PHP 
+Function Date    :  26 Oct 2020
+Function Author  :  Prasad Dangare
+Output           :  0 1 2 3 4
+
+-->
+
+
+<?php
+
+$i = 0;
+
+do {
+    echo $i;
+    $i++;
+} while ($i < 5);
